1b5b4e27d6c6d3b085edf67696a341d859b9ca68
NVHwork <hoang.nv176762@sis.hust.edu.vn>
2022-04-27 10:58:25 +0700

    Split main.cpp from rtspProxyGpu.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-// #include "rtspProxyGpu.hpp"
+#include <fstream>
+#include <cstdlib>
+#include <glib.h>
+#include <gio/gnetworking.h>
+#include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
+#include <gst/rtsp/gstrtspconnection.h>
+#include "rtspProxyGpu.hpp"
 
 #define __ENABLE_OPTIONS
+#define GST_CAT_DEFAULT (onvif_server_debug)
+
+GST_DEBUG_CATEGORY_STATIC(onvif_server_debug);
+
+using json = nlohmann::json;
+using namespace std;
 
 static gchar *port = NULL;
 static gchar *stream_config_file =NULL;
 
 int main(int argc, char *argv[]) {
+	int num_of_streams;
 	GMainLoop *loop;
 	GstRTSPServer *server;
 	GstRTSPMountPoints *mounts;
@@ -19,12 +33,12 @@ int main(int argc, char *argv[]) {
 		//{ "stream-1", 's', 0,  G_OPTION_ARG_STRING, &stream1, "rtsp stream to proxies 1", NULL },
 		//{ "stream-2", 'l', 0,  G_OPTION_ARG_STRING, &stream2, "rtsp stream to proxies 2", NULL },		
 		//{ "username", 'u', 0, G_OPTION_ARG_STRING, &username, "Username for authentication", NULL },
-		//{ "password", 'w', 0, G_OPTION_ARG_STRING, &password, "Password for authentication", NULL },
+		//{ "password", 'd', 0, G_OPTION_ARG_STRING, &password, "Password for authentication", NULL },
 		{ "config", 'c', 0,  G_OPTION_ARG_STRING, &stream_config_file, "Config file for RTSP streaming", NULL },
 #ifdef __ENABLE_OPTIONS
 			//videoscale option
 		#ifdef __CHANGE_RESOLUION
-		//{ "width", 'e', 0, G_OPTION_ARG_INT, &width, "Resize: width", NULL },
+		//{ "width", 'w', 0, G_OPTION_ARG_INT, &width, "Resize: width", NULL },
 		//{ "height", 'h', 0, G_OPTION_ARG_INT, &height, "Resize: height", NULL },
 		#endif
 			//x264enc option
@@ -62,85 +76,16 @@ int main(int argc, char *argv[]) {
 	vector<char> chars(str.c_str(), str.c_str() + str.size() + 1u);
 	port=&chars[0];
 
-	/*
-    // uses at to access fields from json object
-	for(auto &arr: jsonData["streams"]){
-		uri_str[stream_count] = arr.at("stream_uri").get<string>();
-		if (!validate_url(uri_str[stream_count])) {
-			cout << "URL '" << uri_str[stream_count] << "' is not of RTSP type, or wrong format!" << endl << endl;
-			continue;
-		}
-
-		// Parse input codec
-		input_codec_str[stream_count] = get_video_codec(uri_str[stream_count]);
-		if (input_codec_str[stream_count] != NULL) {
-			transcoding_stream[stream_count].inputCodec = input_codec_str[stream_count];
-			cout << "Found video codec: " << input_codec_str[stream_count] << endl << endl;
-		} else {
-			g_printerr("Input video codec not found!\n\n");
-			continue;
-		}
-
-		// Parse output codec
-		output_codec_str[stream_count] = arr.at("output_codec").get<string>();
-		if (input_codec_str[stream_count] != NULL) {
-			transcoding_stream[stream_count].outputCodec = (gchar*)output_codec_str[stream_count].data();
-		} else {
-			g_printerr("Output video codec not found!\n\n");
-			continue;
-		}
-		
-		// Parse stream
-		transcoding_stream[stream_count].stream = (gchar*)uri_str[stream_count].data();
-
-		// Parse bitrate
-		try {
-			transcoding_stream[stream_count].bitrate = stoi(arr.at("bitrate").get<string>());
-		} catch (const exception& e) {
-			cout << e.what() << ". Using camera bitrate setting.\n";
-			transcoding_stream[stream_count].bitrate = 0;
-		}
-
-		// Parse resolution
-		try {
-			transcoding_stream[stream_count].width = stoi(arr.at("width").get<string>());
-		} catch (const exception& e) {
-			cout << e.what() << ". Using camera resolution setting.\n";
-			transcoding_stream[stream_count].width = 0;
-		}
-
-		try {
-			transcoding_stream[stream_count].height = stoi(arr.at("height").get<string>());
-		} catch (const exception& e) {
-			cout << e.what() << ". Using camera resolution setting.\n";
-			transcoding_stream[stream_count].height=0;
-		}
-		
-		stream_count++;
-	}
-
-	if (stream_count <= 0) {
-		g_printerr("NO INPUT STREAMS FOUND!\n");
-		return 0;
-	}
-
-	for (int i=0; i < stream_count; i++){
-		if (transcoding_stream[i].stream == NULL || transcoding_stream[i].outputCodec == NULL){
-			g_printerr("Parsing setting file failed!\n");
-			return 1;
-		}
-		cout << "URL: '" << transcoding_stream[i].stream 
-				<< "', bitrate: " << transcoding_stream[i].bitrate 
-				<< " Mbps, input codec: " << transcoding_stream[i].inputCodec 
-				<< ", output codec: " << transcoding_stream[i].outputCodec 
-				<< ", resolution: " << transcoding_stream[i].width << "x" << transcoding_stream[i].height 
-				<< endl;
-	}
-	if(port==0){
+	if (port == 0) {
 		g_printerr("RTSP Port not setted.\n");
 		return 1;
 	}
-	*/
+	
+	num_of_streams = parse_streams_from_json(jsonData);
+	if (num_of_streams <= 0) {
+		return 1;
+	}
+
 	g_option_context_free(optctx);
 
 	GST_DEBUG_CATEGORY_INIT(onvif_server_debug, "onvif-server", 0, "ONVIF server");
@@ -159,20 +104,20 @@ int main(int argc, char *argv[]) {
 	// 	makeAuth(factory, server);
 	// }
 	
-	for(int i=1;i<=stream_count;i++){
+	for(int i = 1; i <= num_of_streams; i++){
 		char init_uri[] = "/stream-";
 		string s = to_string(i);
-		char *index =(char*) s.c_str(); 
-		//cout<<strcat(init_uri,index)<<endl;
+		char *index =(char*) s.c_str();
 		gst_rtsp_mount_points_add_factory(mounts, (const gchar*)strcat(init_uri,index), factory);
 	}
+
 	g_object_unref(mounts);
 	g_signal_connect(server, "client-connected", G_CALLBACK(on_client_connected), NULL);
 	gst_rtsp_server_attach(server, NULL);
 
 	service = gst_rtsp_server_get_service(server);
-	g_print("Stream ready at:\n");
-	for (int i=1; i<=stream_count; i++) {
+	g_print("\nStream ready at:\n");
+	for (int i = 1; i <= num_of_streams; i++) {
 		char init_uri[] = "/stream-";
 		string s = to_string(i);
 		char *index =(char*) s.c_str(); 
--- rtspProxyGpu.hpp
+++ rtspProxyGpu.hpp
@@ -1,3 +1,12 @@
 #include <iostream>
+#include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
+#include <gst/rtsp/gstrtspconnection.h>
+#include "json.hpp"
 
-int main (int argc, char *argv[]);
+using json = nlohmann::json;
+using namespace std;
+
+int parse_streams_from_json (json jsonData);
+void on_client_connected(GstRTSPServer * server, GstRTSPClient * client, gpointer user_data);
+GstRTSPMediaFactory * onvif_factory_new(void);
